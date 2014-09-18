bb06ca875dedbdb1bbcccbe0b92a40a60be92fd9
David Bayon <dbayon82@googlemail.com>
2014-09-18 15:44:42 +0100

    Machine can be filled with change

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,11 +1,24 @@
 require_relative './cash_container.rb'
 require_relative './product.rb'
 
+STARTING_COINS = { 	"£2" => 10,
+										"£1" => 10,
+										"50p" => 10,
+										"20p" => 15,
+										"10p" => 15,
+										"5p" => 15,
+										"2p" => 20,
+										"1p" => 30 }
+
 class VendingMachine
 	def initialize
-		@cash = CashContainer.new
+		@money = CashContainer.new
 		@products = []
 	end
 
-	attr_reader :cash, :products
+	attr_reader :money, :products
+
+	def fill_with_change(batch)
+		money.accept_coins(batch)
+	end
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,15 +1,22 @@
 require 'vending_machine'
 
 describe 'vending machine' do
-	context 'when initialised' do
-		let(:machine) { VendingMachine.new }
+	let(:machine) { VendingMachine.new }
 
+	context 'when initialised' do
 		it 'has a coin holder' do
-			expect(machine.cash).to be_empty
+			expect(machine.money).to be_empty
 		end
 
 		it 'has an empty product rack' do
 			expect(machine.products).to be_empty
+		end
+	end
+
+	context 'loading the contents' do
+		it 'can be loaded with change' do
+			machine.fill_with_change("£2" => 10, "£1" => 5, "20p" => 3)
+			expect(machine.money.total_value).to eq 25.60
 		end
 	end
 end
